7ea94833d10ac1d600b902c436c88b8da4639d80
League <jiwenzou@gmail.com>
2016-10-10 18:32:59 -0700

    hi

--- src/jake.java
+++ src/jake.java
@@ -1,4 +1,34 @@
 
-public class jake {
+	// Copyright The League of Amazing Programmers 2014
+	import javax.swing.JOptionPane;
 
-}
+	/*
+	 * I have a pocket full of change. I hate doing math. Make me a program that
+	 * will calculate how much money I have without me having to use my brain. Then
+	 * make me a sandwich.
+	 */
+	public class ChangeCalculator {
+
+		public static void main(String[] args) {
+
+			// Ask the user how many nickels they have
+			String numberOfNickles=JOptionPane.showInputDialog(null);
+			// Convert their answer to an int using Integer.parseInt()
+			int nickles=Integer.parseInt(numberOfNickles);
+			// Ask the user how many dimes they have, and convert their answer
+			String numberOfDimes=JOptionPane.showInputDialog(null);
+			int dimes=Integer.parseInt(numberOfDimes);
+			// Ask the user how many quarters they have, and convert their answer
+			String numberOfQuarters=JOptionPane.showInputDialog(null);
+			int quarters=Integer.parseInt(null);
+			String numberOfDollars=JOptionPane.showInputDialog(null);
+			int dollars=Integer.parseInt(null);
+			// Calculate how much money the user has and save it in a double variable 
+
+			// Tell the user how much money they have
+
+		}
+	}
+
+
+
