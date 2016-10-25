afee41bdd687598f14208fb730b2a9ad4923bc40
League <jiwenzou@gmail.com>
2016-10-24 18:30:49 -0700

    f

--- src/gauntlet.java
+++ src/gauntlet.java
@@ -14,8 +14,11 @@ public static void main(String[] args) {
 	//for (int i = 1; i < 100; i=i+2) {
 	//System.out.println(i);	
 	//}
-	for (int i = 0; i < 500; i++) {
-	System.out.println(i);	
-	}
+	
+	
+		
+	}	
+		
+	
 }
-}
+
