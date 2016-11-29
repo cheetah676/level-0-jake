be5a81c26e4069b81f6fdadcba41e6c0c1cbc44e
gerings17 <skyswims@yahoo.com>
2016-11-28 18:28:58 -0800

    hi

---
+++ src/evenOdd.java
@@ -0,0 +1,8 @@
+public class evenOdd {
+	public static void main(String[] args) {
+		for (int i = 1; i < 500; i++) {
+			System.out.println(i * 3);
+		}
+
+	}
+}
--- src/gauntlet.java
+++ src/gauntlet.java
@@ -1,24 +1,21 @@
-import javax.swing.text.StyledEditorKit.ForegroundAction;
-
 public class gauntlet {
-public static void main(String[] args) {
-	//for (int i = 0; i < 101; i++) {
-	//System.out.println(i);	
-	//}
-	//for (int i = 100; i > -1; i--) {
-	//System.out.println(i);	
-	//}
-	//for (int i = 0; i < 101; i= i+2) {
-	//System.out.println(i);	
-	//}
-	//for (int i = 1; i < 100; i=i+2) {
-	//System.out.println(i);	
-	//}
-	
-	
-		
-	}	
-		
-	
-}
+	public static void main(String[] args) {
+		// for (int i = 0; i < 101; i++) {
+		// System.out.println(i);
+		// }
+		// for (int i = 100; i > -1; i--) {
+		// System.out.println(i);
+		// }
+		// for (int i = 0; i < 101; i= i+2) {
+		// System.out.println(i);
+		// }
+		// for (int i = 1; i < 100; i=i+2) {
+		// System.out.println(i);
+		// }
+		for (int i = 0; i < args.length; i++) {
 
+		}
+
+	}
+
+}
