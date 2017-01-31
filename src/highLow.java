9905871b8dd830acdcda912318364f765a13e403
gerings17 <skyswims@yahoo.com>
2017-01-30 18:28:52 -0800

    hi

---
+++ src/highLow.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class highLow {
+	public static void main(String[]args){
+		Random dumpTrump = new Random();
+		int x = dumpTrump.nextInt(100);
+		for (int j = 0; j <7; j++) {
+			
+		String i=JOptionPane.showInputDialog("Try to guess the code!");
+		int guess=Integer.parseInt(i);
+		if (guess==x){
+			JOptionPane.showConfirmDialog(null, "You got it!");
+			j += 1;
+			JOptionPane.showConfirmDialog(null, "It took you "+ j + " tries");
+			break;
+			}
+		else if(guess>x){
+			JOptionPane.showConfirmDialog(null, "HIGH");
+		}
+		else{
+			JOptionPane.showConfirmDialog(null, "LOW");
+		}
+		}
+	}
+}
