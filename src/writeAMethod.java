f1f9b7a6fb119f95af60d19125501bfd50e248bd
zarcut9919 <league@WTS14.attlocal.net>
2017-03-20 18:30:36 -0700

    hi

--- src/writeAMethod.java
+++ src/writeAMethod.java
@@ -1,26 +1,29 @@
 import javax.swing.JOptionPane;
 
 public class writeAMethod {
-public static void main(String[] args){
-	iRock();
-	age(11);	
-	repeatString("HI", 6);
-	multiply(7, 12);
+	public static void main(String[] args) {
+		int x = -3;
+		int y = 6;
+		System.out.println(x + y);
 	}
-	static void iRock(){
-	String name=JOptionPane.showInputDialog("What is your name?");
-	JOptionPane.showConfirmDialog(null, name+" rocks");
-}
-	static void age(int i){
-		JOptionPane.showConfirmDialog(null, 2017-i);
-		
+
+	static void iRock() {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showConfirmDialog(null, name + " rocks");
 	}
-	static void repeatString(String t, int f){
-		for (int i = 1; i <=f; i++) {
-			JOptionPane.showConfirmDialog(null, t);	
+
+	static void age(int i) {
+		JOptionPane.showConfirmDialog(null, 2017 - i);
+
+	}
+
+	static void repeatString(String t, int f) {
+		for (int i = 1; i <= f; i++) {
+			JOptionPane.showConfirmDialog(null, t);
 		}
 	}
-	static void multiply(int k, int y){
-		JOptionPane.showConfirmDialog(null, k*y);
+
+	static void multiply(int k, int y) {
+		JOptionPane.showConfirmDialog(null, k * y);
 	}
 }
